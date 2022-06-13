05b69b2030980db2ca96894ddab89c4e99c8e69c
githun DOHAN25 <joke3323@naver.com>
2022-06-13 15:41:59 +0900

    결제하기 update

--- src/product/index.js
+++ src/product/index.js
@@ -4,10 +4,12 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { API_URL } from "../config/constants";
 import dayjs from "dayjs";
+import { Button, message } from "antd";
+import { AntDesignOutlined } from "@ant-design/icons";
 function ProductPage() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
-  useEffect(function () {
+  const getProduct = () => {
     axios
       .get(`${API_URL}/products/${id}`)
       .then(function (result) {
@@ -16,11 +18,26 @@ function ProductPage() {
       .catch(function (error) {
         console.log(error);
       });
+  };
+  useEffect(function () {
+    getProduct();
   }, []);
   /**이 부분이 없으면 처음에 product 는 null 이기 때문에 error난다. */
   if (product === null) {
     return <h1>상품 정보를 받고 있습니다....</h1>;
   }
+
+  const onClickPurchase = () => {
+    axios
+      .post(`${API_URL}/purchase/${id}`)
+      .then((result) => {
+        message.info("구매가 완료되었습니다.");
+        getProduct();
+      })
+      .catch((error) => {
+        message.error(`에러가 발생했습니다. ${error.message}`);
+      });
+  };
 
   return (
     <div>
@@ -37,6 +54,16 @@ function ProductPage() {
         <div id="createdAt">
           {dayjs(product.createdAt).format("YYYY년 MM월 DD일")}
         </div>
+        <Button
+          id="purchase-button"
+          size="large"
+          type="primary"
+          danger
+          onClick={onClickPurchase}
+          disabled={product.soldout === 1}
+        >
+          구매하기
+        </Button>
         {/*div 태그 사용하면 줄바꿈 적용안된다. 그래서 pre 태그로 변경함*/}
         <pre id="description">{product.description}</pre>
       </div>
