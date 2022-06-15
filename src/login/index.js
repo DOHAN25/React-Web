789f4ab0cdcdb04ecf310933d1e6056f71471b04
githun DOHAN25 <joke3323@naver.com>
2022-06-15 23:07:25 +0900

    loginpage update

--- src/login/index.js
+++ src/login/index.js
@@ -1,11 +1,32 @@
 import "./index.css";
-import { Avatar, Form } from "antd";
+import { Avatar, Form, message } from "antd";
 import { UserOutlined } from "@ant-design/icons";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { API_URL } from "../config/constants";
 
 function LoginPage() {
+  const history = useHistory;
+  const onSubmit = (values) => {
+    axios
+      .post(`${API_URL}/login`, {
+        userId: values.userId,
+        userPassword: values.userPassword,
+      })
+      .then((result) => {
+        console.log(result);
+        message.success("환영합니다.");
+      });
+  };
   return (
     <div id="login-container">
-      <Form></Form>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        autoComplete="on"
+      ></Form>
     </div>
   );
 }
